66c9b158120c3d1cce90d3f3bad11a093aa55841
aqib897 <shekhaqib93@gmail.com>
2026-05-26 02:04:14 +0530

    some changes

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -3,7 +3,7 @@ import { createTransport } from "nodemailer";
 const sendOtp = async (email, subject, otp) => {
   const transporter = createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
 
     auth: {
