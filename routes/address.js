70880ff7290a73e2a64c4f7237eb4a7b1cd21460
Aqib Shekh <shekhaqib93@gmail.com>
2026-05-28 20:43:39 +0530

    Remove duplicate route for getting pincode details

--- routes/address.js
+++ routes/address.js
@@ -11,11 +11,12 @@ import {
 
 const router = express.Router();
 
+router.get("/pincode/:pin", getPincodeDetails);
 router.post("/address/new", isAuth, addAddress);
 router.get("/address/all", isAuth, getAllAddress);
 router.get("/address/:id", isAuth, getSingleAddress);
 router.put("/address/:id", isAuth, updateAddress);
 router.delete("/address/:id", isAuth, deleteAddress);
-router.get("/pincode/:pin", getPincodeDetails);
+
 
 export default router;
