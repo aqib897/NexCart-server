a882bd50eab3a5d87f4aef2a9b0460f3b484f9d9
Aqib Shekh <shekhaqib93@gmail.com>
2026-05-28 19:00:20 +0530

    Update Address.js

--- models/Address.js
+++ models/Address.js
@@ -1,19 +1,78 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const addressSchema =  new mongoose.Schema({
-    address: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: Number,
-        required: true,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
+const addressSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  phone: {
+    type: String,
+    required: true,
+  },
+
+  address1: {
+    type: String,
+    required: true,
+  },
+
+  address2: {
+    type: String,
+  },
+
+  landmark: {
+    type: String,
+  },
+
+  city: {
+    type: String,
+    required: true,
+  },
+
+  state: {
+    type: String,
+    required: true,
+  },
+
+  pincode: {
+    type: String,
+    required: true,
+  },
+
+  country: {
+    type: String,
+    default: "India",
+  },
+
+  type: {
+    type: String,
+    enum: ["Home", "Office", "Other"],
+    default: "Home",
+  },
+
+  isDefault: {
+    type: Boolean,
+    default: false,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export const Address = mongoose.model("Address", addressSchema);
+export const Address = mongoose.model(
+  "Address",
+  addressSchema
+);
